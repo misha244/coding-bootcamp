d4230e1c2d2c08e29b7e0b78f593a3ca8a88943a
Misha <73886737+misha244@users.noreply.github.com>
2021-03-09 21:12:37 +0000

    following along

--- unsolved/week-03/day-01/04-Stu_Hello-Variable/Unsolved/script.js
+++ unsolved/week-03/day-01/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,12 @@
-// Write Your JavaScript Code Here
+var num1 = 2;
+var num2 = 5;
+
+function add(x, y) {
+  var result = x + y;
+
+  console.log(result);
+  return result;
+}
+var result1 = add(2, 5);
+add(num1, num2);
+add(5, 5);

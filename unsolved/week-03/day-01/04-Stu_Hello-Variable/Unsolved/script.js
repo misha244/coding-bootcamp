967e77e736cb0294d8b8b20bffb794335f2886a6
Misha <73886737+misha244@users.noreply.github.com>
2021-03-11 19:23:03 +0000

    git pull

--- unsolved/week-03/day-01/04-Stu_Hello-Variable/Unsolved/script.js
+++ unsolved/week-03/day-01/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,12 +1,20 @@
-var num1 = 2;
-var num2 = 5;
-
-function add(x, y) {
-  var result = x + y;
-
-  console.log(result);
-  return result;
+////var num1 = 2;
+//var num2 = 5;
+//
+//function add(x, y) {
+//  var result = x + y;
+//
+//  console.log(result);
+//  return result;
+//}
+//var result1 = add(2, 5);
+//add(num1, num2);
+//add(5, 5);
+//
+function greet(name, owner) {
+  if (name == owner) {
+    return "Hello boss";
+  } else {
+    return "Hello guest";
+  }
 }
-var result1 = add(2, 5);
-add(num1, num2);
-add(5, 5);
